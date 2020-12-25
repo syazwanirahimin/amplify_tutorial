724683a288c1b69cf8ffa13aa8130589fb040be8
syazwanirahimin <196359@student.upm.edu.my>
2020-12-25 14:23:48 +0800

    Initial commit

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:3479827a-a03d-41cc-8f77-b6b2eaf7e803",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_coF5IwtUN",
+                        "AppClientId": "79s2f13uen2adoqvj9o5urvnd3",
+                        "AppClientSecret": "1sdc3bdjpjnbj02332m6v5hgq5ujjsusvut48c5r9g60u1prpjcv",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
